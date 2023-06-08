08e88acccfb0412a53d96206726d1a3f18011cad
pedrincsharp <joao_pedro_alves_moraes@outlook.com.br>
2023-06-07 21:28:57 -0300

    Feito exercicio de raiz quadrada C

---
+++ C/square_root.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+int square_root(unsigned int value){
+    return sqrt(value);
+}
+
+int main(int argc, char **argv){
+    int resp = square_root(65025);
+    //255, square_root(65025)
+}
